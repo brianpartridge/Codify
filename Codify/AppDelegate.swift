6191e750944fd72a79649a6886b850d242b4b43e
Brian Partridge <brianpartridge@squareup.com>
2016-03-06 10:47:03 -0500

    Initial Commit

---
+++ Codify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Codify
+//
+//  Created by Brian Partridge on 3/6/16.
+//  Copyright © 2016 Pear Tree Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
